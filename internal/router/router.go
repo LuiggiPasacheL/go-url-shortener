8e75fb6b4b7e3af633689ee60be3584fee9cc645
LuiggiPasacheL <luiggi.pasache.lopera@gmail.com>
2025-04-13 17:19:40 -0500

    feat: add middlewares to router

--- internal/router/router.go
+++ internal/router/router.go
@@ -3,12 +3,17 @@ package router
 import (
 	"net/http"
 
+	"github.com/LuiggiPasacheL/go-url-shortener/pkg/log"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter() *gin.Engine {
 
 	r := gin.Default()
+
+    r.Use(gin.Recovery())
+
+    r.Use(log.LoggerMiddleware())
 
 	r.GET("/health", func(c *gin.Context) {
 		c.String(http.StatusOK, "pong")
