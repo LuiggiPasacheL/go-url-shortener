5f7c99a44202b481ce612efbcaeec6759734e81e
LuiggiPasacheL <luiggi.pasache.lopera@gmail.com>
2025-05-25 22:13:06 -0500

    feat: implement get methods in repository

--- internal/repository/url_repository_sqlite.go
+++ internal/repository/url_repository_sqlite.go
@@ -3,7 +3,6 @@ package repository
 import (
 	"context"
 	"database/sql"
-	"errors"
 
 	"github.com/LuiggiPasacheL/go-url-shortener/internal/models"
 )
@@ -31,7 +30,7 @@ func NewUrlRepositorySqlite(file string) (*UrlRepositorySqlite, error){
 }
 
 func (r *UrlRepositorySqlite) Create(ctx context.Context, url *models.Url) (*models.Url, error) {
-	stmt, err := r.db.PrepareContext(ctx, "INSERT INTO urls(url, shortUrl) VALUES ($1, $2) ")
+	stmt, err := r.db.PrepareContext(ctx, "INSERT INTO urls(url, shortUrl) VALUES (?, ?) ")
 	if err != nil {
 		return nil, err
 	}
@@ -59,20 +58,104 @@ func (r *UrlRepositorySqlite) Create(ctx context.Context, url *models.Url) (*mod
 }
 
 func (r *UrlRepositorySqlite) GetById(ctx context.Context, id int) (*models.Url, error) {
-	return nil, errors.New("Not implemented")
+	rows, err := r.db.QueryContext(ctx, "SELECT id, url, shortUrl FROM urls WHERE id = ?", id)
+	if err != nil {
+		return nil, err
+	}
+
+	var sId int
+	var sUrl, sShortUrl string
+	for rows.Next() {
+		err = rows.Scan(&sId, &sUrl, &sShortUrl)
+
+		url := models.Url{
+			Id: &sId,
+			Url: sUrl,
+			ShortUrl: sShortUrl,
+		}
+
+		return &url, nil
+	}
+
+	return nil, sql.ErrNoRows
 }
 
 func (r *UrlRepositorySqlite) GetByShortUrl(ctx context.Context, shortUrl string) (*models.Url, error) {
-	return nil, errors.New("Not implemented")
+	rows, err := r.db.QueryContext(ctx, "SELECT id, url, shortUrl FROM urls WHERE shortUrl = ?", shortUrl)
+	if err != nil {
+		return nil, err
+	}
+
+	var sId int
+	var sUrl, sShortUrl string
+	for rows.Next() {
+		err = rows.Scan(&sId, &sUrl, &sShortUrl)
+
+		url := models.Url{
+			Id: &sId,
+			Url: sUrl,
+			ShortUrl: sShortUrl,
+		}
+
+		return &url, nil
+	}
+
+	return nil, sql.ErrNoRows
 }
 
 func (r *UrlRepositorySqlite) GetAll(ctx context.Context) ([]*models.Url, error) {
-	return nil, errors.New("Not implemented")
+	rows, err := r.db.QueryContext(ctx, "SELECT id, url, shortUrl FROM urls")
+	if err != nil {
+		return nil, err
+	}
+
+	var urls []*models.Url
+	for rows.Next() {
+
+		var sId int
+		var sUrl, sShortUrl string
+		err = rows.Scan(&sId, &sUrl, &sShortUrl)
+		if err != nil {
+			return nil, err
+		}
+
+		url := models.Url{
+			Id: &sId,
+			Url: sUrl,
+			ShortUrl: sShortUrl,
+		}
+
+		urls = append(urls, &url)
+	}
+	if rows.Err() != nil {
+		return nil, err
+	}
+
+	return urls, err
 }
 
 
 func (r *UrlRepositorySqlite) GetByUrl(ctx context.Context, url string) (*models.Url, error) {
-	return nil, errors.New("Not implemented")
+	rows, err := r.db.QueryContext(ctx, "SELECT id, url, shortUrl FROM urls WHERE url = ?", url)
+	if err != nil {
+		return nil, err
+	}
+
+	var sId int
+	var sUrl, sShortUrl string
+	for rows.Next() {
+		err = rows.Scan(&sId, &sUrl, &sShortUrl)
+
+		url := models.Url{
+			Id: &sId,
+			Url: sUrl,
+			ShortUrl: sShortUrl,
+		}
+
+		return &url, nil
+	}
+
+	return nil, sql.ErrNoRows
 }
 
 func (r *UrlRepositorySqlite) Close() error {
